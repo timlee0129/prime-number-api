83004e914f818a04fc8975656a4472f89f1272d0
timlee0129 <timlee0129@gmail.com>
2022-12-25 15:26:35 -0800

    enhanced checkIfPrime endpoint to check for invalid entries, and better tagged response

--- prime_number_api/main.py
+++ prime_number_api/main.py
@@ -13,24 +13,28 @@ app = FastAPI()
 client = MongoClient(config('DB_URI'))
 
 @app.on_event("startup")
-async def startup():
+async def startup() -> None:
     app.mongodb_client = client
     app.database = app.mongodb_client["numbers_db"]
     app.numbers = app.database["numbers"]
 
 @app.on_event("shutdown")
-async def shutdown():
+async def shutdown() -> None:
     await app.mongodb_client.close()
+
+def get_max_prime_entry() -> dict:
+    cursor = app.numbers.find().sort('number',-1).limit(1)
+    return cursor[0]
+
 
 @app.get("/")
 async def get_about() -> dict:
-    cursor = app.numbers.find().sort('number',-1).limit(1)
-    largest_num_entry = cursor[0]
+    max_prime_entry = get_max_prime_entry()
     return {
         "about": "Prime Number API: a self-updating API where you can get basic information about number's prime-ness.",
-        "last_updated": largest_num_entry['_id'].generation_time.strftime('%Y-%m-%d'),
-        "max_prime_number": largest_num_entry['number'],
-        "max_prime_order": largest_num_entry['order']
+        "last_updated": max_prime_entry['_id'].generation_time.strftime('%Y-%m-%d'),
+        "max_prime_number": max_prime_entry['number'],
+        "max_prime_order": max_prime_entry['order']
     }
 
 @app.get("/checkIfPrime")
@@ -39,7 +43,10 @@ async def check_prime(num: List[str] = Query(default=None)):
         num = [int(i) for i in num]
     except:
         raise HTTPException(400)
+    max_prime_entry = get_max_prime_entry()
+    max_prime_number = max_prime_entry['number']
     for idx, val in enumerate(num):
+        assert val > 1 and val <= max_prime_number, "Invalid number entry. Number must be >1 and <= current max prime"
         cursor = app.numbers.find_one({"number":{"$eq":val}},{"_id": 0})
-        num[idx] = json.loads(dumps(cursor))
+        num[idx] = {val: json.loads(dumps(cursor))}
     return num
