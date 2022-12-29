593592d8fc841221eea25a8fb5ba20a5da692417
timlee0129 <timlee0129@gmail.com>
2022-12-28 16:06:16 -0800

    adding primeNumbers endpoint

--- prime_number_api/main.py
+++ prime_number_api/main.py
@@ -26,6 +26,10 @@ def get_max_prime_entry() -> dict:
     cursor = app.numbers.find().sort('number',-1).limit(1)
     return cursor[0]
 
+def get_min_prime_entry() -> dict:
+    cursor = app.numbers.find().sort('number',1).limit(1)
+    return cursor[0]
+
 @app.get("/")
 async def get_about() -> dict:
     max_prime_entry = get_max_prime_entry()
@@ -37,16 +41,96 @@ async def get_about() -> dict:
     }
 
 @app.get("/checkIfPrime")
-async def check_prime(num: List[str] = Query(default=None)):
+async def check_prime(num: List[str] = Query(default=...)):
     try:
         num = [int(i) for i in num]
     except:
         raise HTTPException(400)
     max_prime_entry = get_max_prime_entry()
     max_prime_number = max_prime_entry['number']
+    invalid_entries = [val for val in num if val < 2 or val > max_prime_number]
+    if len(invalid_entries) > 0:
+        raise HTTPException(status_code=400,detail=f"Invalid number entries: {invalid_entries}. Numbers must be >1 and <={max_prime_number} (current max prime)")
+    query_dict = {val['number']: val for val in list(app.numbers.find({"number":{"$in":num}},{"_id": 0}))}
     for idx, val in enumerate(num):
-        if val < 2 or val > max_prime_number:
-            raise HTTPException(status_code=400,detail=f"Invalid number entry: {val}. Numbers must be >1 and <= current max prime")
-        cursor = app.numbers.find_one({"number":{"$eq":val}},{"_id": 0})
-        num[idx] = {val: json.loads(dumps(cursor))}
+        num[idx] = {val: query_dict.get(val)}
     return num
+
+@app.get("/primeNumbers")
+async def get_prime_numbers(
+    q_order: Union[int, None] = Query(
+        default = 0,
+        alias="order",
+    ),
+    q_type: Union[str, None] = Query(
+        default = "number",
+        alias="type",
+    ),
+    q_min: Union[int, None] = Query(
+        default = 2,
+        alias="min",
+    ),
+    q_max: Union[int, None] = Query(
+        default = None,
+        alias="max",
+    ),
+    q_len: Union[int, None] = Query(
+        default = 1,
+        alias="len",
+    ),
+):
+    # verify that inputs are valid
+    valid_order = [-1,0,1]
+    valid_type = ["number", "order"]
+    max_prime = get_max_prime_entry().get(q_type)
+    min_prime = get_min_prime_entry().get(q_type)
+    if q_max is None:
+        q_max = max_prime
+    max_len = 1000
+    if q_order not in valid_order:
+        raise HTTPException(status_code=400,detail=f"Invalid 'order' input: {q_order}. 'order' must be one of these: {valid_order}")
+    if q_type not in ["number", "order"]:
+        raise HTTPException(status_code=400,detail=f"Invalid 'type' input: {q_type}. 'type' must be one of these: {valid_type}")
+    if q_min < min_prime:
+        raise HTTPException(status_code=400,detail=f"Invalid 'min' input: {q_min}. 'min' must be >{min_prime} for type '{q_type}'")
+    if q_max > max_prime:
+        raise HTTPException(status_code=400,detail=f"Invalid 'max' input: {q_max}. 'max' must be <={max_prime} for type '{q_type}'")
+    if q_len > 1000:
+        raise HTTPException(status_code=400,detail=f"Invalid 'len' input: {q_len}. 'len' must be <={max_len}")
+
+    # if order is random
+    if q_order == 0:
+        return list(app.numbers.aggregate([
+                {
+                    "$match": {
+                        q_type: {
+                            "$gte": q_min,
+                            "$lte": q_max,
+                        }
+                    }
+                },
+                {
+                    "$sample": {
+                        "size": q_len
+                    }
+                },
+                {
+                    "$project": {
+                        "_id": 0
+                    }
+                }
+            ])
+        )
+    else:
+        return list(app.numbers.find(
+                {
+                    q_type: {
+                        "$gte": q_min,
+                        "$lte": q_max,
+                    }
+                },
+                {
+                    "_id": 0
+                }
+            ).sort(q_type,q_order).limit(q_len)
+        )
